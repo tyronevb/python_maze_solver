155a64dd6beede13efab7e93d38f167dd269b97e
Tyrone van Balla <tyronevb@live.com>
2025-04-28 12:36:00 +0200

    add cell class to create and draw maze cells

---
+++ cell.py
@@ -0,0 +1,36 @@
+from graphics import Point, Line
+
+class Cell(object):
+    def __init__(self, x1, y1, x2, y2, window):
+        """
+        x1, y1 - cords of top-left corner
+        x2, y2 - cords of bottom-right corner
+        """
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = window
+    
+    def draw(self):
+        top_left = Point(self._x1, self._y1)
+        bottom_right = Point(self._x2, self._y2)
+        top_right = Point(self._x2, self._y1)
+        bottom_left = Point(self._x1, self._y2)
+
+        if self.has_left_wall:
+            left_wall_line = Line(top_left, bottom_left)
+            self._win.draw_line(left_wall_line)
+        if self.has_right_wall:
+            right_wall_line = Line(top_right, bottom_right)
+            self._win.draw_line(right_wall_line)
+        if self.has_top_wall:
+            top_wall_line = Line(top_left, top_right)
+            self._win.draw_line(top_wall_line)
+        if self.has_bottom_wall:
+            bottom_wall_line = Line(bottom_left, bottom_right)
+            self._win.draw_line(bottom_wall_line)
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from graphics import Window, Point, Line
+from cell import Cell
 
 win = Window(800, 600)
+cell_1 = Cell(10, 10, 30, 30, win)
+cell_1.draw()
 
-point_a = Point(1,1)
-point_b = Point(800,600)
-line_a_b = Line(point_a, point_b)
-
-point_c = Point(800,0)
-point_d = Point(0,600)
-line_c_d = Line(point_c, point_d)
-
-win.draw_line(line_a_b, "red")
-win.draw_line(line_c_d, "blue")
-
+cell_2 = Cell(80, 10, 90, 400, win)
+cell_2.has_bottom_wall = False
+cell_2.draw()
 win.wait_for_close()
